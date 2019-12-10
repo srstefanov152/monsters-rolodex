73c39f1530a71db4730a8fb0ee6df4f8e06a0a0f
srstefanov152 <s.r.stefanov@abv.bg>
2019-12-10 13:58:40 +0200

    Updates

---
+++ precache-manifest.808783fb9ce456d0da1ce9ffebc0d7a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a34b9f4dd0b4608f74498f11e823bac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62666f762fe941ee5e43",
+    "url": "/monsters-rolodex/static/css/main.86608600.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters-rolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "62666f762fe941ee5e43",
+    "url": "/monsters-rolodex/static/js/main.63959810.chunk.js"
+  },
+  {
+    "revision": "03497ef441aebc7cc47b",
+    "url": "/monsters-rolodex/static/js/runtime-main.666613d3.js"
+  }
+]);
